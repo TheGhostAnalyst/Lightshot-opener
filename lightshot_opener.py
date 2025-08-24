3da708c5c71d656e159b86d23833057c8533e30f
The Ghost Analyst <habibshittu8@gmail.com>
2025-08-23 22:45:31 -0400

    Update lightshot_opener.py

--- lightshot_opener.py
+++ lightshot_opener.py
@@ -1,15 +1,43 @@
 import random
 import string
 import webbrowser
-for i in range(5):
+import pyinputplus as Pyip
+from pathlib import Path
+from datetime import datetime
 
-    a = string.ascii_lowercase
-    a1 = ( ''.join(random.choice(a) for i in range(2)))
- 
-    b = string.digits
-    b1 = ( ''.join(random.choice(b) for i in range(4)))
- 
-    url = "https://prnt.sc/"
- 
-    link = url+a1+b1
-    webbrowser.open(link)
+all_links = []
+try:
+    num_links = Pyip.inputInt("How many links do you want to generate: ")
+
+    for i in range(num_links):
+        a1 = ''.join(random.choice(string.ascii_lowercase) for _ in range(2))
+        b1 = ''.join(random.choice(string.digits) for _ in range(4))
+    
+        link = (f"https://prnt.sc/{a1}{b1}")
+        all_links.append(link)
+        print(f'Link Generated: {link}')
+    print('Link generated above:')
+    save = Pyip.inputStr("\nDo you want to save these links to a file? (yes/no): ").strip().lower()
+    if save in ['yes', 'y']:
+        with open('all_links.txt', 'a') as f:
+            now = datetime.now()
+            timestamp = now.strftime('%Y-%m-%d %H:%M')
+            f.write(f"######################################\n{timestamp}\n")
+            for link in all_links:
+                f.write(f"{link}\n")
+        the_path = Path('all_links.txt').resolve()
+        print(f'\nYour generated links have been saved in {the_path} ')
+    else:
+        print("Links have not been saved.")
+
+    open_in_browser = Pyip.inputStr("Do you want to open these links in your web browser? (yes/no): ").strip().lower()
+    if open_in_browser in ['yes', 'y']:
+        for link in all_links:
+            webbrowser.open(link)
+        print("Links have been opened in your web browser.")
+    else:
+        print("Links have not been opened.")
+except KeyboardInterrupt:
+    print('\nProcess interrupted. Exiting...')
+except Exception as e:
+    print(f"An error occurred: {e}")
